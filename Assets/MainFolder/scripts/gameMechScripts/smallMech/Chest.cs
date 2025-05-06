76ac8f810f49d94ef8f8b17d264700009d24f783
Sodnom1994 <93848263+Sodnom1994@users.noreply.github.com>
2025-05-06 07:36:37 +0300

    1.7.2

--- Assets/MainFolder/scripts/gameMechScripts/smallMech/Chest.cs
+++ Assets/MainFolder/scripts/gameMechScripts/smallMech/Chest.cs
@@ -1,16 +1,30 @@
+using System.Collections;
 using UnityEngine;
 [RequireComponent(typeof(BoxCollider2D))]   
 public class Chest : MonoBehaviour, IInteractable
 {
     [SerializeField] private GameObject coinPrefab;
     [SerializeField] private Transform dropPoint;
+    [SerializeField] private Animator animator;
+    private bool isAnimatorSet => animator != null;
     public void Interact()
     {
+        if (isAnimatorSet)
+        {
+            animator.SetTrigger("isOpen");
+        }
         Debug.Log("Сундук открылся");
-        if(coinPrefab != null && dropPoint != null)
+        if (coinPrefab != null && dropPoint != null)
         {
             Instantiate(coinPrefab, dropPoint.position, Quaternion.identity);
         }
+        StartCoroutine(CloseChest());
+
     }
+    IEnumerator CloseChest()
+    {
+        yield return new WaitForSeconds(3f);
+        animator.SetTrigger("isClose");
+    }    
 }
 
--- Assets/MainFolder/scripts/gameMechScripts/smallMech/Coin.cs
+++ Assets/MainFolder/scripts/gameMechScripts/smallMech/Coin.cs
@@ -1,18 +1,33 @@
 using UnityEngine;
-[RequireComponent (typeof(CircleCollider2D))]
+[RequireComponent(typeof(CircleCollider2D))]
+[RequireComponent(typeof(Rigidbody2D))]
 public class Coin : MonoBehaviour, ICollectable
 {
+    private Rigidbody2D coinRigidbody;
+    [SerializeField] private float throwForce = 10f;
+    private void Start()
+    {
+        if (coinRigidbody != null)
+        {
+            Vector2 randomUpSemiSphereDirection = new Vector2(
+                Random.Range(-0.5f, 0.5f),
+                Random.Range(0.5f, 1f)
+                );
+            coinRigidbody.AddForce(randomUpSemiSphereDirection * throwForce, ForceMode2D.Impulse);
+        }
+    }
     public void Collect()
     {
         Debug.Log("Монетка поднята");
         gameObject.SetActive(false);
+
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.gameObject.CompareTag("Player"))
+        if (collision.gameObject.CompareTag("Player"))
         {
             Collect();
-            Destroy(gameObject);
+
         }
     }
 }
